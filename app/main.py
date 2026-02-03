8c4d54d792efe63b6cef33a5fe68c7dbc523365d
CodeAnthonyy <Anthony.0145.23@gmail.com>
2026-02-02 21:53:37 -0300

    funções de adicionar produtos e itens e suas respectivas telas em html

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from database.engine import init_db
+from database.engine import add_product
+from database.engine import add_item
 
 app = Flask(__name__)
 
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,27 @@
 from main import app
 from flask import render_template
+from database.engine import get_products_with_items
+from database.engine import search_products
+from flask import request
 
-# Rota principal
+
+# Rota tela inicial
 @app.route("/")
 def home():
     return render_template("base.html")
+
+#rota para buscar produtos
+@app.route("/products")
+def list_products():
+    data = get_products_with_items()
+    return render_template("products.html", data=data)
+
+#DEFINIR
+@app.route("/produtos")
+def produtos():
+
+    search = request.args.get("search", "")
+
+    data = search_products(search)
+
+    return render_template("products.html", data=data)
